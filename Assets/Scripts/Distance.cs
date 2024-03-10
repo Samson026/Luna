d5f44ec10cfe7f17af6bc6fea3639e14b826e5d5
Samuel Dietz <samdietz2001@gmail.com>
2024-03-10 18:26:49 +1100

    POC

---
+++ Assets/Scripts/Distance.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Distance : MonoBehaviour
+{
+    public LibPdInstance pdPatch;
+    public Transform targetLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (Vector3.Distance(targetLocation.position, transform.position) * 1000);
+        Debug.Log("distance: " + distance);
+        pdPatch.SendFloat("distance", distance);
+    }
+}
---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject followingMe; // in the inspector drag the gameobject the will be following the player to this field
+    public int followDistance;
+    private List<Vector3> storedPositions;
+ 
+ 
+    void Awake()
+    {
+        storedPositions = new List<Vector3>(); //create a blank list
+     
+        if(!followingMe)
+        {
+            Debug.Log("The FollowingMe gameobject was not set");
+        }      
+     
+        if(followDistance == 0)
+        {
+            Debug.Log("Please set distance higher then 0");
+        }
+    }
+ 
+    void Start ()
+    {
+ 
+    }
+ 
+    void Update()
+    {
+        storedPositions.Add(transform.position); //store the position every frame
+     
+        if(storedPositions.Count > followDistance)
+        {
+            followingMe.transform.position = storedPositions[0]; //move the player
+            storedPositions.RemoveAt (0); //delete the position that player just move to
+        }
+    }
+}
