ebaaf4a70252c76416f65ba02e10c15102d27e23
Samuel Dietz <samdietz2001@gmail.com>
2024-03-16 15:33:12 +1100

    Updated pd and added new synth

--- Assets/Scripts/Distance.cs
+++ Assets/Scripts/Distance.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,29 @@ using UnityEngine;
 public class Distance : MonoBehaviour
 {
     public LibPdInstance pdPatch;
-    public Transform targetLocation;
+    public Transform leftHand;
+    public Transform rightHand;
     // Start is called before the first frame update
     void Start()
     {
-        
+        pdPatch.SendFloat("harm", 9.0f);
+        pdPatch.SendFloat("note", 7.0f);
+        pdPatch.SendFloat("detune", 0.0f);
+        pdPatch.SendFloat("vol", 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = (Vector3.Distance(targetLocation.position, transform.position) * 1000);
-        pdPatch.SendFloat("distance", distance);
+       
+        // pdPatch.SendFloat("ampmod", 1.0f);
+        float leftHandDistance = Vector3.Distance(leftHand.position, transform.position);
+        leftHandDistance = leftHandDistance * 16;
+        float rightHandDistance = Vector3.Distance(rightHand.position, transform.position);
+        rightHandDistance = rightHandDistance * 100;
+        Debug.Log(rightHandDistance);
+        Debug.Log(leftHandDistance);
+        pdPatch.SendFloat("ampmod", rightHandDistance);
+        pdPatch.SendFloat("harm", leftHandDistance);
     }
 }
