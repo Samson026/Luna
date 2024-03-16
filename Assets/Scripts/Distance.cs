93c0069c16fd03a920ac7b7a22b7eb6ad8150d2b
Samuel Dietz <samdietz2001@gmail.com>
2024-03-16 19:00:09 +1100

    Implemented object grabbing
    * Left and right hands now control aspects of the synth

--- Assets/Scripts/Distance.cs
+++ Assets/Scripts/Distance.cs
@@ -1,13 +1,20 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Oculus.Interaction.HandGrab;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Distance : MonoBehaviour
 {
     public LibPdInstance pdPatch;
     public Transform leftHand;
     public Transform rightHand;
+    private bool grabbed = false;
+    private Vector3 gPosition;
+    public HandGrabInteractor [] interactors;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,15 +27,36 @@ public class Distance : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Check if object is grabbed
+        bool aHand = false;
+        for (int i = 0; i < interactors.Length; i++) {
+            if (interactors[i].State == Oculus.Interaction.InteractorState.Select) {
+                aHand = true;
+            }
+        }
+
+        if (aHand) {
+            if (!grabbed) {
+                grabbed = true;
+                gPosition = transform.position;
+            }
+        }
+        else
+            grabbed = false;
        
         // pdPatch.SendFloat("ampmod", 1.0f);
-        float leftHandDistance = Vector3.Distance(leftHand.position, transform.position);
-        leftHandDistance = leftHandDistance * 16;
-        float rightHandDistance = Vector3.Distance(rightHand.position, transform.position);
-        rightHandDistance = rightHandDistance * 100;
-        Debug.Log(rightHandDistance);
-        Debug.Log(leftHandDistance);
-        pdPatch.SendFloat("ampmod", rightHandDistance);
-        pdPatch.SendFloat("harm", leftHandDistance);
+        if (grabbed) {
+            float leftHandDistance = Vector3.Distance(leftHand.position, gPosition);
+            leftHandDistance = leftHandDistance * 16;
+            float rightHandDistance = Vector3.Distance(rightHand.position, gPosition);
+            rightHandDistance = rightHandDistance * 100;
+            if (rightHandDistance < 10)
+                rightHandDistance = 1.0f;
+            Debug.Log("right " + rightHandDistance);
+            Debug.Log("left " + leftHandDistance);
+            Debug.Log("pos" + gPosition);
+            pdPatch.SendFloat("ampmod", rightHandDistance);
+            pdPatch.SendFloat("harm", leftHandDistance);
+        }
     }
 }
