ceff07dc7a3f55e5344c1b678c094d775b596776
Samuel Dietz <samdietz2001@gmail.com>
2024-03-25 13:53:45 +1100

    Added spatial audio

--- Assets/Scripts/Distance.cs
+++ Assets/Scripts/Distance.cs
@@ -14,14 +14,14 @@ public class Distance : MonoBehaviour
     public Transform rightHand;
     private bool grabbed = false;
     private Vector3 gPosition;
+    public string name;
     public HandGrabInteractor [] interactors;
     // Start is called before the first frame update
     void Start()
     {
-        pdPatch.SendFloat("harm", 9.0f);
-        pdPatch.SendFloat("note", 7.0f);
-        pdPatch.SendFloat("detune", 0.0f);
-        pdPatch.SendFloat("vol", 0.3f);
+        pdPatch.SendFloat("harm"+name, 3.0f);
+        pdPatch.SendFloat("note"+name, 3.0f);
+        pdPatch.SendFloat("vol", 0.2f);
     }
 
     // Update is called once per frame
@@ -47,16 +47,16 @@ public class Distance : MonoBehaviour
         // pdPatch.SendFloat("ampmod", 1.0f);
         if (grabbed) {
             float leftHandDistance = Vector3.Distance(leftHand.position, gPosition);
-            leftHandDistance = leftHandDistance * 16;
+            leftHandDistance = leftHandDistance * 50;
             float rightHandDistance = Vector3.Distance(rightHand.position, gPosition);
-            rightHandDistance = rightHandDistance * 100;
-            if (rightHandDistance < 10)
-                rightHandDistance = 1.0f;
+            // rightHandDistance = rightHandDistance;
+            // if (rightHandDistance < 10)
+            //     rightHandDistance = 1.0f;
             Debug.Log("right " + rightHandDistance);
             Debug.Log("left " + leftHandDistance);
             Debug.Log("pos" + gPosition);
-            pdPatch.SendFloat("ampmod", rightHandDistance);
-            pdPatch.SendFloat("harm", leftHandDistance);
+            pdPatch.SendFloat("M"+name, rightHandDistance);
+            pdPatch.SendFloat("I"+name, leftHandDistance);
         }
     }
 }
