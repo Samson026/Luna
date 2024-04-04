da45454fc6ab2656a606128a419eb57e5d6c61c6
Samuel Dietz <samdietz2001@gmail.com>
2024-04-04 16:36:01 +1100

    Added delay to sphere movement

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -16,6 +16,7 @@ public class SphereController : MonoBehaviour
     private bool grabbed = false;
     private bool attached = false;
     private Vector3 gPosition;
+    private List<Vector3> positions;
     public HandGrabInteractable interactables;
     // Start is called before the first frame update
     void Start()
@@ -44,27 +45,26 @@ public class SphereController : MonoBehaviour
             }
         }
        
-        // pdPatch.SendFloat("ampmod", 1.0f);
         if (attached) {
             // send audio data
 
-
-
-            // float leftHandDistance = Vector3.Distance(leftHand.position, gPosition);
-            // leftHandDistance = leftHandDistance * 50;
-            // float rightHandDistance = Vector3.Distance(rightHand.position, gPosition);
-            // Debug.Log("right " + rightHandDistance);
-            // Debug.Log("left " + leftHandDistance);
-            // Debug.Log("pos" + gPosition);
-            // pdPatch.SendFloat("M", rightHandDistance);
-            // pdPatch.SendFloat("I", leftHandDistance);
             pdPatch.SendFloat("ampmod", Gap() * 100);
             pdPatch.SendFloat("note", Distance() * 8);
             pdPatch.SendFloat("I", Angle() * 50);
 
             // update position
             Vector3 pos = (rightHand.position + leftHand.position) /2;
-            transform.position = pos;
+            positions.Add(pos);
+
+            Debug.Log(positions.Count());
+
+            if (positions.Count() >= 20) {
+                transform.position = positions[0];
+                positions.RemoveAt(0);
+            }
+        }
+        else {
+            positions = new List<Vector3>();
         }
     }
     
@@ -77,10 +77,6 @@ public class SphereController : MonoBehaviour
         float y = rightHand.position.y - leftHand.position.y; 
         float z = rightHand.position.z - leftHand.position.z;
 
-        // float mx = y/x;
-        // float my = z/y;
-        // float mz = z/x;
-        // float t = System.MathF.Pow(mx, 2f) + System.MathF.Pow(my, 2f) + System.MathF.Pow(mz, 2f);
         float m = y/x;
         float angleRadians = Mathf.Atan(m);
         float angleDegrees = angleRadians * Mathf.Rad2Deg;
