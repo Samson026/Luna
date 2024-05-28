163d65ba31b7554286cacd41040c72426391559a
Samuel Dietz <samdietz2001@gmail.com>
2024-05-28 11:20:35 +1000

    Final fixes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,7 +15,6 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         spheres = new List<GameObject>();
-        attachedSphere = new GameObject();
     }
 
     // Update is called once per frame
@@ -25,22 +24,27 @@ public class GameManager : MonoBehaviour
     }
 
     public void SpawnSphere() {
-        GameObject s = Instantiate(sphere);
+        Vector3 pos = (leftHand.transform.position + rightHand.transform.position) /2;
+        GameObject s = Instantiate(sphere, pos, transform.rotation);
         s.GetComponent<SphereController>().rightHand = rightHand;
         s.GetComponent<SphereController>().leftHand = leftHand;
-        s.GetComponent<SphereController>().startPosition = s.transform.position;
-        // Debug.Log("running Creating " + s);
         spheres.Add(s);
-        // Debug.Log("running Creating " + spheres.First());       
+        s.GetComponent<SphereController>().AttachSphere();
+        attachedSphere = s;     
     }
 
     public void AttachSphere() {
-        // Debug.Log("running attachSphere");
+        GameObject s;
         Transform rightHandPos = this.transform;
         float minDistance = float.MaxValue;
-        GameObject s = spheres.First();
 
-        // Debug.Log("running len" + spheres.Count);
+        if (attachedSphere != null) {
+            attachedSphere.GetComponent<SphereController>().AttachSphere();
+            attachedSphere = null;
+            return;
+        }
+
+        s = spheres.First();
 
         foreach (var bone in rightHand.GetComponent<OVRSkeleton>().Bones) {
             if (bone.Id == OVRSkeleton.BoneId.Hand_Thumb1) {
@@ -50,14 +54,11 @@ public class GameManager : MonoBehaviour
 
         foreach (var sphere in spheres) {
             float distance  = Vector3.Distance(sphere.transform.position, rightHandPos.position);
-            // Debug.Log("running dis " + distance + " " + minDistance );
             if (distance < minDistance) {
-                // Debug.Log("running settings a sphere");
                 minDistance = distance;
                 s = sphere;
             }
         }
-        // Debug.Log("running attach2 + " + s);
         s.GetComponent<SphereController>().AttachSphere();
         attachedSphere = s;
     }
@@ -65,6 +66,6 @@ public class GameManager : MonoBehaviour
     public void DeleteSphere() {
         spheres.Remove(attachedSphere);
         attachedSphere.GetComponent<SphereController>().Remove();
-        attachedSphere = new GameObject();
+        AttachSphere();
     }
 }
